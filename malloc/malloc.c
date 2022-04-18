3b0fa9c9615dda62df22320c7ffefc27b4e81477
Arjan van de Ven <arjan@linux.intel.com>
2022-04-19 00:19:29 +0900

    malloc_tune.patch
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- malloc/malloc.c
+++ malloc/malloc.c
@@ -916,7 +916,7 @@ libc_hidden_proto (__libc_mallopt)
 #define M_TRIM_THRESHOLD       -1
 
 #ifndef DEFAULT_TRIM_THRESHOLD
-#define DEFAULT_TRIM_THRESHOLD (128 * 1024)
+#define DEFAULT_TRIM_THRESHOLD (512 * 1024)
 #endif
 
 /*
@@ -949,7 +949,7 @@ libc_hidden_proto (__libc_mallopt)
 #define M_TOP_PAD              -2
 
 #ifndef DEFAULT_TOP_PAD
-#define DEFAULT_TOP_PAD        (0)
+#define DEFAULT_TOP_PAD        (64 * 1024)
 #endif
 
 /*
