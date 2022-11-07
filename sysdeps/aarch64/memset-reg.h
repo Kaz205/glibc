562a3d82e089a066f5b1db38983ace983b357178
Kazuki Hashimoto <kazukih@tuta.io>
2022-11-07 21:56:17 +0900

    arm64: Update memset.S
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- sysdeps/aarch64/memset-reg.h
+++ sysdeps/aarch64/memset-reg.h
@@ -22,9 +22,4 @@
 #define count	x2
 #define dst	x3
 #define dstend	x4
-#define tmp1	x5
-#define tmp1w	w5
-#define tmp2	x6
-#define tmp2w	w6
-#define zva_len x7
-#define zva_lenw w7
+#define zva_val	x5
